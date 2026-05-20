b18d4b8559a6d508c9d914733eff59f6b6bf86af
sarovar <sarovarlearner@gmail.com>
2026-05-20 17:05:18 +0530

    Intentional break to test CI

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+gdg
 app.UseHttpsRedirection();
 
 var summaries = new[]
@@ -33,7 +33,7 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
-app.Run();
+app.Runn();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
